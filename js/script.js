450c09ffd6bacbc4906cb7f59086af331924ad52
rohbdn07 <57314666+rohbdn07@users.noreply.github.com>
2020-01-13 13:20:01 +0200

    added some javscript

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log("this is me");
+
+var a=1;
+var b=2;
+
+if(a<b){
+    console.log("a is lesser than b");
+}else{
+    console.log("a is greater than b");
+}
+if(a===2){
+    console.log("a is greater than b");
+}
+
